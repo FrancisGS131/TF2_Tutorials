c924b009d017b36c73fbadd4e2a5d8cd73edbd90
Francis (Ubuntu) <syfrancis131@gmail.com>
2021-01-07 20:28:05 -0500

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/francis/tutorial_ws/src
+# Build directory: /home/francis/tutorial_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_setup_tf")
+subdirs("learning_tf2")
